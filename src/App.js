f2b7bdfa6f5303edce2d523eaf277387b59fe5be
Mukaila Salami <cmovieconf@gmail.com>
2025-03-24 21:03:21 -0600

    creating template for first subapp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import App1 from './apps/App1/App1';
+import IdentifyThis from './apps/identify-this/src/IdentifyThis';
 import App2 from './apps/App2/App2';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/apps/App1/App1" element={<App1 />} />
+        <Route path="/apps/identify-this/IdentifyThis" element={<IdentifyThis />} /> 
         <Route path="/apps/App2/App2" element={<App2 />} />
       </Routes>
     </Router>
--- src/apps/App1/App1.jsx
+++
@@ -1,4 +0,0 @@
-export default function App1() {
-    return <h2>This is Mini App 1</h2>;
-  }
-  
