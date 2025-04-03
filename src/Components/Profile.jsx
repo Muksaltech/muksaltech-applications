c2fffbfd4ccb190b9ccee50078893cb02507bfe3
Mukaila Salami <cmovieconf@gmail.com>
2025-04-02 20:33:16 -0600

    updating profile page

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -15,11 +15,11 @@ export default function Profile() {
                     {/* Main content */}
                     <main className="w-full md:w-2/3 p-4 rounded-lg shadow">
                         <p className="text-gray-300 leading-relaxed">
-                        Hello! I'm Mukaila — a passionate developer with over 20 years experience in building web and mobile applications, as well as engaging side projects. I have a strong appreciation for clean design and intuitive user experiences.
+                        Hello! I'm Mukaila — a passionate developer with over 20 years experience in building web and mobile applications, as well as engaging in  challenging open source projects. I have a strong appreciation for clean design and intuitive user experiences.
 
                         Guided by my personal motto — “Constantly creating, constantly developing, constantly revamping, and constantly innovating” — I'm always working on something new. Below is a list of apps I'm currently developing.
 
-                        Each project has been built entirely from scratch, and you're welcome to explore the code. You can click on yhe burger icon to see the list of the features I demoed in each app.
+                        Each project has been built entirely from scratch, and you're welcome to explore the code. You can click on the burger icon to see the list of the features I demoed in each app.
 
                         My past clients include GE Oil and Gas, USAA bank, PayPal, AvNet, AT&T, APS, PGE and Ihop demonstrating my ability to work across different domains.
                         </p>
