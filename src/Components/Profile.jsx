2dede866763354ce22e6caff439259090b23d74f
Mukaila Salami <cmovieconf@gmail.com>
2025-04-06 18:28:28 -0600

    Updating profile page

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,19 +1,23 @@
+import me from '../assets/images/me.webp'
+
 export default function Profile() {
     return <>
-        <article className="max-w-5xl mx-auto border border-gray-300 p-6 rounded-lg">
-                <header className="mb-6 text-center">
+        <article className="max-w-5xl mx-auto p-6 rounded-lg">
+                <header className="text-center mb-6">
                 <h1 className="text-3xl font-bold text-gray-300">About Me</h1>
                 </header>
 
-                <div className="flex flex-col md:flex-row gap-6">
+                <div className="flex flex-col  gap-6">
 
-                    {/* Sidebar / Profile */}
-                    <aside className="w-full md:w-1/3 h-full  rounded-lg shadow-inner">
-                    <img alt="" src={null} />
-                    </aside>
+                {/* Sidebar / Profile  */}
+                <aside className = "flex items-start justify-center">
+                    <img alt="me" src={me} className="w-[13rem] h-[13rem] object-contain rounded-lg shadow-inner" />
+                </aside>
+               
+           
 
                     {/* Main content */}
-                    <main className="w-full md:w-2/3 p-4 rounded-lg shadow">
+                <main className="w-full rounded-lg shadow text-justify">
                         <p className="text-gray-300 leading-relaxed">
                         Hello! I'm Mukaila — a passionate developer with over 20 years experience in building web and mobile applications, as well as engaging in  challenging open source projects. I have a strong appreciation for clean design and intuitive user experiences.
 
