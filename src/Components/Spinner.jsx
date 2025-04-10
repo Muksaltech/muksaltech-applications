0b0ec2fca40ab1f252c66431e812b61622050564
Mukaila Salami <cmovieconf@gmail.com>
2025-04-09 21:51:39 -0600

    hosting SQL DB Data on Azure Cloud

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,7 @@
+const Spinner = () => (
+  <div className="flex justify-center items-end h-32">
+    <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-gray-300"></div>
+  </div>
+);
+
+export default Spinner;
--- src/apps/food-order/src/FoodOrder.jsx
+++ src/apps/food-order/src/FoodOrder.jsx
@@ -1,29 +1,28 @@
-import logo from './logo.svg';
-import './FoodOrder.css';
+import { useEffect, useState } from 'react';
 import BurgerList from './components/BurgerList';
-
-import FireBite from '../src/assets/firebite.jpg'
-import { useEffect , useState} from 'react';
+import Spinner from '../../../Components/Spinner';
+import './FoodOrder.css';
 
 function FoodOrder() {
+  const getBurgersDataFromAzureCloud =
+    'https://muksalfoodorder-g7htfpcpdvccdaec.canadacentral-01.azurewebsites.net/api/burgers';
 
-  const getBurgersData = `http://localhost:8080/api/burgers`;
-  const [burgersData, setBurgersData] = useState([{}]);
-
- // let burgersData = [];
+  const [burgersData, setBurgersData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchBurgers = async () => {
       try {
-        const response = await fetch(getBurgersData);
+        const response = await fetch(getBurgersDataFromAzureCloud);
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        setBurgersData(data)
-       //burgersData = [...burgersData, data];
+        setBurgersData(data);
       } catch (error) {
-        console.error("Error fetching burgers:", error);
+        console.error('Error fetching burgers:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -31,22 +30,24 @@ function FoodOrder() {
   }, []);
 
   return (
-      <div className="px-4 py-8">
+    <div className="px-4 py-8">
       <h2 className="text-2xl font-semibold text-center mb-6">Muksal Fictitious Burger Joint</h2>
       <p className="text-center mb-8 text-gray-400 max-w-xl mx-auto">
-          Discover The Latest Flavors At Muksal! No Matter What Time Of Day, Our Amazing Bugers Are Here To Satisfy Any Craving.
-        </p>
+        Discover The Latest Flavors At Muksal! No Matter What Time Of Day, Our Amazing Burgers Are Here To Satisfy Any Craving.
+      </p>
 
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        {
-          burgersData.map(burgerdata => (
-            <BurgerList key={burgerdata.burgerId} burgerData={burgerdata} />
-          ))
-
-        }
-      </div>
-      
-      </div>
+      {loading ? (
+        <div className="flex justify-center mt-28">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          {burgersData.map((burgerData) => (
+            <BurgerList key={burgerData.burgerId} burgerData={burgerData} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
 
