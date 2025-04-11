7a93a5f2f760d307fdf241c7b58fb5cdf1275203
Mukaila Salami <cmovieconf@gmail.com>
2025-04-10 18:20:38 -0600

    color adjustment

--- src/apps/food-order/src/components/BurgerSidesHeading.jsx
+++ src/apps/food-order/src/components/BurgerSidesHeading.jsx
@@ -3,7 +3,7 @@ function BurgerSidesHeading({ title }) {
   
     return (
         <>
-            <h4 className="text-2xl mt-4 font-semibold text-gray-300 mb-2">{title} <span className="text-red-600">*</span></h4>
+            <h4 className="text-2xl mt-4 font-semibold text-gray-300 mb-2">{title} <span className="text-gray-400">*</span></h4>
         </>
      );
 }
