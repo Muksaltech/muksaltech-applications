83d075357c4e7c9be0aa74fc2a6bbb758dc04aed
Mukaila Salami <cmovieconf@gmail.com>
2025-04-10 18:10:39 -0600

    changing spinner appearance

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -1,6 +1,6 @@
 const Spinner = () => (
-  <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-    <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
+  <div className="fixed inset-0 flex items-center justify-center z-50">
+    <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-grey-300"></div>
   </div>
 );
 
